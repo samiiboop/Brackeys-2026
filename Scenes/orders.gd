62920a43c036e0007186c20c7947dcb9ffb5dce7
Sammy Sherifi <sammysherifi@gmail.com>
2026-08-26 18:32:30 -0400

    fijoifjs

--- Scenes/orders.gd
+++ Scenes/orders.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 """ Handles creating new orders, expiring them and spawning in new servers"""
 
+## TO DO: Difficulty scaling, creating an order etc etc
 const FOOD = preload("uid://cha2c65a22fex")
+
+# References.
+@onready var order_timer: Timer = $OrderTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#order_timer.timeout.connect()
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_order_timer_timeout():
 	pass
