c3987fa98c2867900d8c39d24c10f433d3c8f166
Rose <sammysherifi@gmail.com>
2026-08-27 14:00:24 -0400

    more stuff

--- Scenes/Orders/food.gd
+++ Scenes/Orders/food.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var rich_text_label: RichTextLabel = $MarginContainer/MarginContainer/RichTextLabel
+
 """
 This defines what the food requires to be complete and scored
 
@@ -9,31 +11,63 @@ ehh: Required ingredient in, not required added
 bad: No required ingredient in 
 """
 
-# Script vars.
-var sweet_required : int = 0
-var sour_required : int = 0
-var salty_required : int = 0
-var umami_required : int = 0
-var bitter_required : int = 0
-
+# A list of all possible requirements
+var requirements = {
+	"sweet" : 0,
+	"sour" : 0 ,
+	"salty" : 0, 
+	"umami" : 0, 
+	"bitter" : 0
+}
+# A list of stuff THIS order requires, we will use this to hold thier values
+var order_requirements = {}
+var possible_strings = [
+	"Customer says there needs to be something %s in the %s please fix so I can go home..."
+]
+var dialog_string = ""
+var amount_string = ""
 # References
 @onready var server_sprite: Sprite2D = $ServerSprite
 @onready var deposit_area: Area2D = $DepositArea
 
-func _init(cheese_level) -> void:
-	print("IM ALIVE", cheese_level)
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
 
+func setup(slot):
+	global_position = slot.global_position
+	generate_order()
+	
 
-func setup_server():
-	pass
+func generate_order():
+	# Pick a random sprite
+	# Based on global difficulty decide how many ingredients it needs
+	if Game.difficulty <= 5:
+		generate_easy()
+	else:
+		return
+
+func generate_easy():
+	# get a random requirement
+	var requirement = requirements.keys().pick_random()
+	# generate a random amount
+	var amount = randi_range(1, 5)
+	# get the amount string for the dialog box
+	match amount:
+		1:
+			amount_string = "a pinch"
+		2:
+			amount_string = "a little"
+		3:
+			amount_string = "a decent amount"
+		4:
+			amount_string = "quite a bit"
+		5:
+			amount_string = "something really strong"
+	order_requirements[requirement] = amount
+	dialog_string = possible_strings.pick_random() % [requirement, "chicken"]
+	rich_text_label.text = dialog_string
 
 func deliver_dialog():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	pass 
--- Scenes/orders.gd
+++ Scenes/orders.gd
@@ -5,13 +5,36 @@ extends Node2D
 ## TO DO: Difficulty scaling, creating an order etc etc
 const FOOD = preload("uid://cha2c65a22fex")
 
+@onready var server_pos_1: Marker2D = $ServerPos1
+@onready var server_pos_2: Marker2D = $ServerPos2
+@onready var server_pos_3: Marker2D = $ServerPos3
+
+
+var server_pos_occupance : Dictionary = {
+	server_pos_1 : false,
+	server_pos_2 : false,
+	server_pos_3: false 
+}
+
+
 # References.
 @onready var order_timer: Timer = $OrderTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#order_timer.timeout.connect()
-	pass
+	order_timer.timeout.connect(_on_order_timer_timeout)
+	order_timer.start()
 
 func _on_order_timer_timeout():
+	var food_scene = FOOD.instantiate()
+	add_child(food_scene)
+	# get an unoccupied slot for them to go into
+	for slot in server_pos_occupance.keys():
+		if server_pos_occupance[slot] == true: return
+		print("DO WE HAVE A ", slot)
+		food_scene.setup($ServerPos1)
+		server_pos_occupance[slot] = true
+
+# Dynamically connected when we spawn in a server in on order timeout
+func _on_order_complete():
 	pass
